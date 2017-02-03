dd7e4fa202a277a017000a0325490fc631216738
zzsoszz <137573155@qq.com>
2017-02-03 20:44:22 +0800

    update

--- src/main/java/com/blogspot/sgdev/blog/Application.java
+++ src/main/java/com/blogspot/sgdev/blog/Application.java
@@ -10,7 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAutoConfiguration()
 @EnableResourceServer
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
